070b5c4762c59de5dc639f3e9369dbac2477fcff
lrj <1849590021@qq.com>
2022-03-18 11:02:35 +0800

    添加了随机种子的设定,保证可复现

--- main.py
+++ main.py
@@ -8,18 +8,21 @@ from model import make_model
 from train import trainer
 from test import test
 from logger import make_logger
+from utils.set_seed import setup_seed
 
+setup_seed(0)
 
 class CONFIG:
     DATASET_PATH = '/Users/lurenjie/Documents/dataset/Skin40'
     SIZE = (224, 224)
     # model
     # train
+    DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    LR = 0.0001
     TRAIN_BATCH_SIZE = 32
     EPOCH_NUM = 100
-    LR = 0.0001
+    EVALUATE_INTERVAL = 10
     SAVE_INTERVAL = 100
-    EVALUATE_INTERVAL = 5
     # test
     TEST_BATCH_SIZE = 100
 
@@ -38,24 +41,23 @@ def train():
         cfg.TRAIN_BATCH_SIZE, cfg.TEST_BATCH_SIZE)
     for fold, (train_loader, val_loader, test_loader) in enumerate(dataloaders):
         fold += 1
-        model = make_model()
+        model = make_model().to(cfg.DEVICE)
         loss_fn = nn.CrossEntropyLoss()
         optimizer = optim.Adam(model.fc.parameters(), lr=cfg.LR)
 
         logger.info(f'fold{fold} start trainning')
         for epoch in range(cfg.EPOCH_NUM):
-            epoch += 1
-            loss, train_accuracy = trainer(model, train_loader, loss_fn, optimizer)
+            loss, train_accuracy = trainer(model, train_loader, loss_fn, optimizer, cfg.DEVICE)
 
             train_data.loc[epoch]['loss'] = loss
             train_data.loc[epoch]['train_accuracy'] = train_accuracy
 
             logger.info(
                 'fold[%d/5] epoch[%d/%d] loss: %f train accuracy: %f' % (
-                fold, epoch, cfg.EPOCH_NUM, loss, train_accuracy))
+                fold, epoch + 1, cfg.EPOCH_NUM, loss, train_accuracy))
 
             if (epoch + 1) % cfg.EVALUATE_INTERVAL == 0:
-                val_accuracy = test(model, val_loader)
+                val_accuracy = test(model, val_loader, cfg.DEVICE)
                 train_data.loc[epoch][f'fold{fold}']=val_accuracy
                 logger.info('val accuracy: %f' % (val_accuracy))
 
@@ -63,8 +65,8 @@ def train():
                 torch.save(model.state_dict(), f'logs/model_fold{fold}_{epoch}.pth')
                 train_data.to_csv(f'train_data_{epoch}.csv')
         # 测试该fold下的模型准确率
-        test_accuracy = test(model, test_loader)
-        logger.info('------------------------')
+        test_accuracy = test(model, test_loader, cfg.DEVICE)
+        logger.info('-------------------------')
         logger.info('test accuracy: %f' % (test_accuracy))
     # finished trainning
     logger.info('finished trainning')
